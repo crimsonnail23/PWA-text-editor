bfbf7d0deaabc6975afa5671845bfd0923d1bd90
night <yseddiq@gmail.com>
2022-10-27 04:07:42 -0400

    forgot to save changes to last commit, babel-loader was added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,6 +51,18 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options:{
+              presets:[
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
         }
       ],
     },
